52786989d674def7c53b41835c96200f1e61711a
DuncanHallum <rc23386@bristol.ac.uk>
2025-07-08 18:09:44 +0100

    Rewrite prompts
    Change to generate belief distributions seperately, call update_belief twice with different context passed as parameter.

--- model/main.py
+++ model/main.py
@@ -1,13 +1,15 @@
 import os
 import openai
 from dotenv import load_dotenv
+import matplotlib.pyplot as plt
+import json
+from pathlib import Path
+
+BASE_PATH = Path(__file__).parent.parent
+print(BASE_PATH)
 
 load_dotenv()
-
 api_key = os.getenv("OPENAI_API_KEY")
-
-
-#openai.apikey = os.environ.get("OPENAI_API_KEY")
 
 # 28 emotions of Go Emotions data set
 EMOTIONS = [
@@ -21,8 +23,8 @@ EMOTIONS = [
 # State is 2 lists: user's emotion and user's believed emotion of another character
 STATES = [EMOTIONS, EMOTIONS]
 
-# Generates uniform distribution over states
-def generate_init_belief(states): 
+# Generates 2 uniform distribution over states
+def generate_init_beliefs(states): 
     belief = [{},{}]
     prob = 1/len(states[0]) # uniform 
     for state in states[0]:
@@ -32,9 +34,9 @@ def generate_init_belief(states):
 
 def recognise_character(text):
     response = openai.chat.completions.create(
-        model="gpt-4o-mini",
+        model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": f"You are an NER model. Identify the name of the character that the user is talking about, give only the name of the character and nothing else."},
+            {"role": "system", "content": f"You are a named entity recognotion model. Identify the name of the character that the user is talking about, give only the name of the character and nothing else."},
             {"role": "user", "content": text}
         ],
         max_tokens=1000,
@@ -42,24 +44,18 @@ def recognise_character(text):
     )
     return response.choices[0].message.content
 
-def update_belief(belief, observation, character, prev_action=None):
+def update_belief(observation, context, prev_action=None):
     response = openai.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
             {"role": "system", "content": f"""
                                         You are part of a cognitive model, reasoning about peoples mental states in
-                                        workplace environment following a POMDP structure. Your job is to update the belief distribution.
+                                        workplace environment following a POMDP structure. Your job is to adjust the current belief distribution.
                                         
-                                        Given the user's input as an observation, the current belief, and the previous action, generate a new 
-                                        belief distribution as an array of 2 dictionaries, the 1st mapping the user's mental state to a probability, 
-                                        the 2nd mapping the user's belief of {character}'s mental state to a probability.
-
-                                        The current belief distribution is {belief}, where the first dictionary represents
-                                        the probability distribution of the user's mental states, and the second distribution 
-                                        represents that of the user's belief about {character}'s mental state.
+                                        Given the user's input as an observation, the current belief, and the previous action, update the belief distribution to include this new context as a JSON object mapping each of these mental states {EMOTIONS} to {context}
                                         Your previous action or message to the user was {prev_action} if available.
 
-                                        Output only the 2D list of the belief, with 2 non-empty disctionaries each with the 28 mental states.
+                                        Output only the dictionary mapping each state to a probability.
              """},
             {"role": "user", "content": observation}
         ],
@@ -74,12 +70,12 @@ def generate_action(belief, observation, character):
         messages=[
             {"role": "system", "content": f"""
                                         You are a cognitive model, used to give advice on workplace problems and relations with a POMDP structure.
-                                        The current belief distribution is {belief}, where the first dictionary represents
-                                        the probability distribution of the user's mental states, and the second distribution
-                                        represents that of the user's belief about {character}'s mental state.
+                                        Each belief distribution maps a state to a probability.
+                                        The current belief distribution of the user's mental state is {belief[0]}.
+                                        The current belief distribution of which mental state the user thinks that {character} is in is {belief[1]}.
 
-                                        Given this information, generate an action from the follwing user message, in the form of giving helpful advice, asking a question to
-                                        better your understand without being too invasive, or give a sympathetic response.
+                                        Given this information, generate an action from the following user message, in the form of giving helpful advice, asking a question to
+                                        better your understand without being too invasive, or give a sympathetic or encouraging response.
              """},
             {"role": "user", "content": observation}
         ],
@@ -93,11 +89,18 @@ if __name__ == "__main__":
     user_input = ""
     while user_input != "end":
         observation = input("User: ")
-        belief = generate_init_belief(STATES)
+        beliefs = generate_init_beliefs(STATES)
         character = recognise_character(observation)
         print(character)
-        belief = update_belief(belief, observation, character, action)
-        print(belief)
-        action = generate_action(belief, observation, character)
+
+        context_users_state = f"the probability that the user is that state. The current belief distribution is {beliefs[0]}."
+        belief_user_state = json.loads(update_belief(observation, context_users_state, action)) #belief distribution of user's own mental state
+
+        context_character_state = f"the probability of the user thinking that {character} is in that state. The current belief distribution is {beliefs[1]}."
+        belief_character_state = json.loads(update_belief(observation, context_character_state, action)) #belief distribution of the mental state which the user believes the other character is in
+       
+        beliefs = [belief_user_state, belief_character_state]
+        #Plot belief distributions
+        action = generate_action(beliefs, observation, character)
         print(action)
 
